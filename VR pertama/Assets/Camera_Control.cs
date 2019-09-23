35cac90f7e3579b1991a0763a1448ba62b5a6139
stradzzz <salzulal@yahoo.com>
2019-09-23 16:55:47 +0700

    tambah mobil, maps 1 its

--- VR pertama/Assets/Camera_Control.cs
+++ VR pertama/Assets/Camera_Control.cs
@@ -14,13 +14,17 @@ public class Camera_Control : MonoBehaviour
 
     public float minimumVert = -45.0f;
     public float maximumVert = 45.0f;
+    public float minimumHor = -45.0f;
+    public float maximumHor = 45.0f;
+
 
     //sensitivity
     public float sensHorizontal = 10.0f; 
     public float sensVertical = 10.0f;
 
     //where player looking
-    public float _rotationX = 0;
+    public float _rotationX;
+    public float _rotationY;
 
 
     // Update is called once per frame
@@ -34,8 +38,12 @@ public class Camera_Control : MonoBehaviour
             _rotationX -= Input.GetAxis("Mouse Y") * sensVertical;
             _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert); //set vertical angle within max an min limit (clamp)
 
-            float rotationY = transform.localEulerAngles.y;
-            transform.localEulerAngles = new Vector3 (_rotationX, rotationY, 0);
+            _rotationY = Input.GetAxis("Mouse X") * sensHorizontal;
+            _rotationY = Mathf.Clamp(_rotationY, minimumHor, maximumHor); //set horizontal angle within max an min limit (clamp)
+
+
+//            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3 (_rotationX, _rotationY, 0);
         }
     }
 }
