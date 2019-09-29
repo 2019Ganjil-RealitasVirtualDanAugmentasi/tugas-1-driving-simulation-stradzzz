ac4487d1f9e211b3729b163e451ee4d9a338bc1c
stradzzz <salzulal@yahoo.com>
2019-09-29 23:40:46 +0700

    fixed camera rotate & add collider

--- VR pertama/Assets/Camera_Control.cs
+++ VR pertama/Assets/Camera_Control.cs
@@ -4,23 +4,16 @@ using UnityEngine;
 
 public class Camera_Control : MonoBehaviour
 {
-    public enum RotationAxis
-    {
-        MouseX = 1,
-        MouseY = 2
-    }
 
-    public RotationAxis axes = RotationAxis.MouseX;
-
-    public float minimumVert = -45.0f;
-    public float maximumVert = 45.0f;
-    public float minimumHor = -45.0f;
-    public float maximumHor = 45.0f;
+    public float minimumVert = -25.0f;
+    public float maximumVert = 25.0f;
+    public float minimumHor = -80.0f;
+    public float maximumHor = 80.0f;
 
 
     //sensitivity
-    public float sensHorizontal = 10.0f; 
-    public float sensVertical = 10.0f;
+    public float sensHorizontal = 5.0f; 
+    public float sensVertical = 5.0f;
 
     //where player looking
     public float _rotationX;
@@ -30,20 +23,15 @@ public class Camera_Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(axes == RotationAxis.MouseX)
-        {
-            transform.Rotate (0, Input.GetAxis("Mouse X") * sensHorizontal, 0);
-        } else if (axes == RotationAxis.MouseY)
-        {
+   
             _rotationX -= Input.GetAxis("Mouse Y") * sensVertical;
             _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert); //set vertical angle within max an min limit (clamp)
 
-            _rotationY = Input.GetAxis("Mouse X") * sensHorizontal;
+            _rotationY -= Input.GetAxis("Mouse X") * sensHorizontal;
             _rotationY = Mathf.Clamp(_rotationY, minimumHor, maximumHor); //set horizontal angle within max an min limit (clamp)
 
 
-//            float rotationY = transform.localEulerAngles.y;
-            transform.localEulerAngles = new Vector3 (_rotationX, _rotationY, 0);
-        }
+            transform.localEulerAngles = new Vector3(_rotationX, -_rotationY, 0);
+        
     }
 }
